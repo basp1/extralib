77c2bbc5ff20ef621585e596a9a8f619bceaeaf7
basp <akovn@yandex.ru>
2018-02-01 16:16:43 +0200

    add default annotations

---
+++ src/main/java/com/github/basp1/extralib/Immutable.java
@@ -0,0 +1,12 @@
+package com.github.basp1.extralib;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Target({java.lang.annotation.ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Immutable {
+}
